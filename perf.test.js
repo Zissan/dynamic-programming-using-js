3e7403b9de8f58555ff72f75cff0d8f28eabf8ec
zissan.chowdhury@yahoo.com <zissan.chowdhury@yahoo.com>
2022-04-16 21:55:12 +0530

    introduced performance calculator

--- perf.test.js
+++ perf.test.js
@@ -8,12 +8,12 @@ const testEqual = (message, testMethod) => {
     }
 }
 
-const sumOfNNumbers = (...params) => {
+const sumOfNNumbers = (params = []) => {
     return params.reduce((acc, param) => acc + param, 0);
 }
 
 const testFunctionWithPerf = perf(sumOfNNumbers);
 
-const params = Array.from({ length: 100 }).map((_, index) => index);
+const params = Array.from({ length: 10000000 }).map((_, index) => index);
 
-testEqual('Should return 4950', testFunctionWithPerf(...params) === 4950);
+testEqual('Should return 49999995000000', testFunctionWithPerf(params) === 49999995000000);
