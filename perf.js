2f9aed1ada9f8765d8ec552a828989bbd2272d3a
zissan.chowdhury@yahoo.com <zissan.chowdhury@yahoo.com>
2022-04-16 23:39:04 +0530

    added unit to the execution time

--- perf.js
+++ perf.js
@@ -5,7 +5,7 @@ const perf = (func, thisArg) => {
         const result = func.apply(thisArg, params);
         const endTime = +(new Date());
         console.log('***PROCESS COMPLETED***');
-        console.log(`EXECUTION TIME: ${endTime - startTime}`);
+        console.log(`EXECUTION TIME: ${endTime - startTime} ms`);
         return result;
     }
 }
